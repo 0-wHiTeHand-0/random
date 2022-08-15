dc54c882a268ed4ab2f78e4e6548b9a6be91cd15
Saul <saul@cryptolab.net>
2022-08-15 17:22:29 -0500

    Update send.js

--- chat_AWS_Lambda/send.js
+++ chat_AWS_Lambda/send.js
@@ -1,22 +1,9 @@
+"use strict";
 const AWS = require('aws-sdk');
-
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: '<REGION>' });
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: 'eu-west-3' });
 
 exports.handler = async event => {
   console.log('New message from ' + event.requestContext.identity.sourceIp + ': ' + event.body);
-  let connectionData;
-  
-  try {
-    connectionData = await ddb.scan({ TableName: '<TABLENAME>', ProjectionExpression: '<ID>' }).promise();
-  } catch (e) {
-    console.log(e);
-    return { statusCode: 500, body: '{"username":"Error","message":"Error"}' };
-  }
-  
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-  });
   
   let postData;
   try {
@@ -31,15 +18,28 @@ exports.handler = async event => {
     console.log(e);
     return { statusCode: 400, body: '{"username":"Error","message":"Input error"}' };
   }
+  
+  let connectionData;
+  try {
+    connectionData = await ddb.scan({ TableName: 'chat-connections', ProjectionExpression: 'connectionId' }).promise();
+  } catch (e) {
+    console.log(e);
+    return { statusCode: 500, body: '{"username":"Error","message":"Error"}' };
+  }
+  
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  });
 
-  const postCalls = connectionData.Items.map(async ({ <ID> }) => {
-    if (<ID> != event.requestContext.<ID>){
+  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+    if (connectionId != event.requestContext.connectionId){
       try {
-        await apigwManagementApi.postToConnection({ ConnectionId: <ID>, Data: postData }).promise();
+        await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
       } catch (e) {
         if (e.statusCode === 410) {
-          console.log(`Found stale connection, deleting ${<ID>}`);
-          await ddb.delete({ TableName: '<TABLENAME>', Key: { <ID> } }).promise();
+          console.log(`Found stale connection, deleting ${connectionId}`);
+          await ddb.delete({ TableName: 'chat-connections', Key: { connectionId } }).promise();
         } else {
           throw e;
         }
