822c567b14e867675b28d309a4a21bfb357fb059
Saul <saul@cryptolab.net>
2022-08-15 17:28:44 -0500

    Update connect.js

--- chat_AWS_Lambda/connect.js
+++ chat_AWS_Lambda/connect.js
@@ -1,19 +1,22 @@
+"use strict";
 const AWS = require('aws-sdk');
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: '<REGION>' });
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: 'eu-west-3' });
 
 exports.handler = async event => {
-  const putParams = {
-    TableName: '<TABLENAME>',
-    Item: {
-      <ID>: event.requestContext.<ID>
-    }
-  };
-  
   if (event.headers['Origin'] != '<YOURORIGIN>'){
     console.log('Bad Origin from: ' + event.requestContext.identity.sourceIp);
     return { statusCode: 400, body: 'Bad Origin.' };
   }
+
+  const putParams = {
+    TableName: 'chat-connections',
+    Item: {
+      connectionId: event.requestContext.connectionId,
+      timetolive: Math.round((Date.now()/1000)+(8*3600))
+      //timetolive: Math.round(Date.now()/1000)+(15)
+    }
+  };
 
   try {
     await ddb.put(putParams).promise();
